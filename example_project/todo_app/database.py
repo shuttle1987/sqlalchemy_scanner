ed65aa5e5f316cc345062d38464021e5f45e510a
Janis <consulting@lesinskis.com>
2021-07-28 01:47:54 +1000

    SQLAlchemy setup

--- example_project/todo_app/database.py
+++ example_project/todo_app/database.py
@@ -2,3 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+engine = create_engine("sqlite:///./test.db", connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()

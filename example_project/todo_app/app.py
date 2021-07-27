2eba8fd15333284525b9d82fdfe54ca4f7ae820c
Janis <consulting@lesinskis.com>
2021-07-28 02:45:03 +1000

    Create a helper for converting a SQLAlchemy model to dictionary

--- example_project/todo_app/app.py
+++ example_project/todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import flask, _app_ctx_stack
+from flask import flask, _app_ctx_stack, jsonify
 from sqlalchemy.orm import scoped_session
 
 from . import models
@@ -10,3 +10,9 @@ app.session = scoped_session(SessionLocal, scopefunc=_app_ctx_stack.__ident_func
 @app.teardown_appcontext
 def remove_session(*args, **kwargs):
     app.session.remove()
+
+
+@app.route("/tasks/")
+def show_tasks():
+    tasks = app.session.query(models.Task).all()
+    return jsonify([task.to_dict() for task in tasks])
--- example_project/todo_app/models.py
+++ example_project/todo_app/models.py
@@ -5,7 +5,18 @@ from sqlalchemy.types import Date
 
 from .database import Base
 
-
+class DictMixIn:
+    """Convenience mixin for creating a JSON representation for a SQL row result
+    where that row is defined by a SQLAlchemy model"""
+    def to_dict(self) -> dict:
+        return {
+            column.name: getattr(self, column.name)
+            if not isinstance(
+                getattr(self, column.name), (datetime.datetime, datetime.date)
+            )
+            else getattr(self, column.name).isoformat()
+            for column in self.__table__.columns
+        }
 
 class TaskStatusEnum(enum.Enum):
     not_started = 1
@@ -19,7 +30,7 @@ def string_to_taskstatusenum(s: str) -> TaskStatusEnum:
     return res
 
 
-class Task(Base):
+class Task(Base, DictMixIn):
     """DB model for a TODO list task"""
 
     __tablename__ = "tasks"

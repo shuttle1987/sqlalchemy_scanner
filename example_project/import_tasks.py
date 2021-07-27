d60cef3a5c336c1223ed56a7877bd215823857c3
Janis <consulting@lesinskis.com>
2021-07-28 02:24:40 +1000

    Make a data importing script

---
+++ example_project/import_tasks.py
@@ -0,0 +1,25 @@
+"""Import some data into the app using CSV files"""
+
+import csv
+import datetime
+
+from app import models
+from app.database import SessionLocal, engine
+
+if __name__ == "__main__":
+    try:
+        db = SessionLocal()
+
+        with open("tasks.csv", "r") as f:
+            csv_reader = csv.DictReader(f)
+            for row in csv_reader:
+                task = models.Task(
+                    title=row["title"],
+                    description=row["description"],
+                    due_date=row["due_date"],
+                    status=row["status"],
+                )
+                db.add(task)
+        db.commit()
+    finally:
+        db.close()

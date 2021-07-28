018d8d7f23b092cea20562d0d274ed29f7a9f2df
Janis <consulting@lesinskis.com>
2021-07-28 19:10:37 +1000

    function to run development server

--- example_project/admin.py
+++ example_project/admin.py
@@ -1,6 +1,10 @@
+from todo_app.add import app
 from todo_app.database import Base, engine
 
 def create_tables():
     """Create database tables for the app"""
     Base.metadata.create_all(engine)
 
+def run_dev_server(port: int=5000):
+    """run the development server"""
+    app.run(port=port)

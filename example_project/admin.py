f69739e63cb2c2d71a301a5bc01c35d51808bb95
Janis <consulting@lesinskis.com>
2021-07-28 19:59:24 +1000

    Create some command line parsing options to spin up dev server

--- example_project/admin.py
+++ example_project/admin.py
@@ -1,4 +1,6 @@
-from todo_app.add import app
+import argparse
+
+from todo_app.app import app
 from todo_app.database import Base, engine
 
 def create_tables():
@@ -8,3 +10,15 @@ def create_tables():
 def run_dev_server(port: int=5000):
     """run the development server"""
     app.run(port=port)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default=5000, type=int)
+
+    parser.add_argument('--run-dev-server', dest='run_dev', action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    if args.run_dev:
+        port_arg = args.port
+        run_dev_server(port=port_arg)

0e68356e9b50dce1d1d92a86ec945d8403c201e6
Janis <consulting@lesinskis.com>
2021-07-28 20:11:26 +1000

    Add some arguments to create DB tables

--- example_project/admin.py
+++ example_project/admin.py
@@ -5,7 +5,9 @@ from todo_app.database import Base, engine
 
 def create_tables():
     """Create database tables for the app"""
+    print("Creating database tables using SQLAlchemy ORM")
     Base.metadata.create_all(engine)
+    print("Done creating tables")
 
 def run_dev_server(port: int=5000):
     """run the development server"""
@@ -15,9 +17,25 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', default=5000, type=int)
 
-    parser.add_argument('--run-dev-server', dest='run_dev', action='store_true', default=False)
+    parser.add_argument(
+        '--run-dev-server',
+        dest='run_dev',
+        action='store_true',
+        default=False,
+        help="Run the flask development server",
+    )
+    parser.add_argument(
+        '--create-tables',
+        dest='create_tables',
+        action='store_true',
+        default=False,
+        help="Create the database tables",
+    )
 
     args = parser.parse_args()
+
+    if args.create_tables:
+        create_tables()
 
     if args.run_dev:
         port_arg = args.port
